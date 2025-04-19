62eb7ccee25c5177cbf38857bed9a240808bb190
mario <mariojosechavezferreyra@gmail.com>
2025-04-19 22:18:42 +0300

    quest 1 funcion division natural añadida

--- src/1948.c
+++ src/1948.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
-
-int division_natural;
-
+int division_natural(int dividendo,int divisor);
 int main(){
-    int num;
+    int num1;
+    int num2;
     char c;
-    if (scanf("%d%c", &num, &c) == 2 && c == '\n'){
-
+    if (scanf("%d%d%c", &num1,&num2, &c) == 3 && c == '\n'){
+    int resultado = division_natural(num1,num2);
+    printf("%d\n",resultado);
     } else {
     printf("n/a\n");
     }
 return 0;
 }
+// creamos un metodo para dividir sin utilizar el operador "/" en C
+// la division natural te devuelve el numero entero del resultado, si es 7/2 = 3
+int division_natural(int dividendo,int divisor){
+  int contador = 0;
+  while(dividendo > 0){ //100
+    dividendo = dividendo - divisor; // dividendo - divisor es lo mismo que 100 - 10
+                                    // y el dividendo de la izquierda sera
+                                    // el valor de 100 - 10 que es igual a 90
+    contador++; // contador = contador + 1;
+  }
+  return contador;
+}
+
+// funcion para saber si un numero primo o no (un numero primo es primo si solo se puede
+// dividir entre 1 y si mismo)
+// crear una funcion para saber si un numero es primo o no es primo utilizando la funcion
+// que hemos creado.
