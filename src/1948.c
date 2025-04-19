068afa048dc6d3db750971742b7e67647d5403da
GabriloChFe <gabrielchavezferreyra@gmail.com>
2025-04-19 19:58:00 +0100

    Prueba Quest 1

---
+++ src/1948.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int division_natural;
+
+int main(){
+    int num;
+    char c;
+    if (scanf("%d%c", &num, &c) == 2 && c == '\n'){
+
+    } else {
+    printf("n/a\n");
+    }
+return 0;
+}
