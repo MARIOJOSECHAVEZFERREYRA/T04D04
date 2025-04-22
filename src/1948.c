7be18bbba2e47cb69115c99b3d696e6b8ee34a96
mario <mariojosechavezferreyra@gmail.com>
2025-04-22 23:10:27 +0300

    quest 1todavia no hecho

--- src/1948.c
+++ src/1948.c
@@ -14,18 +14,28 @@ return 0;
 }
 // creamos un metodo para dividir sin utilizar el operador "/" en C
 // la division natural te devuelve el numero entero del resultado, si es 7/2 = 3
-int division_natural(int dividendo,int divisor){
+int division_natural(int dividendo,int divisor){ // dividendo / divisor
   int contador = 0;
-  while(dividendo > 0){ //100
+  do{
     dividendo = dividendo - divisor; // dividendo - divisor es lo mismo que 100 - 10
                                     // y el dividendo de la izquierda sera
-                                    // el valor de 100 - 10 que es igual a 90
-    contador++; // contador = contador + 1;
-  }
-  return contador;
-}
+                                   // el valor de 100 - 10 que es igual a 90
 
+    contador++; // contador = contador + 1;
+  } while(dividendo > 0);
+  return contador;
+    /* 10 2
+  1. 10 - 2 = 8 > 0
+  2. 8 - 2 = 6 > 0
+  3. 6 - 2 = 4 > 0
+  4. 4 - 2  = 2 > 0
+  5. 2 -2 = 0
+  6. 0 > 0
+  */
+}
 // funcion para saber si un numero primo o no (un numero primo es primo si solo se puede
 // dividir entre 1 y si mismo)
 // crear una funcion para saber si un numero es primo o no es primo utilizando la funcion
 // que hemos creado.
+
+
